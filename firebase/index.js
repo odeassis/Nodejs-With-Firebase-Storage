2bdd5233b93a2509603d7f3e2b9849fba0ea6cf2
fdAssis <fd07ass@gmail.com>
2020-09-12 23:47:06 -0300

    upload image route

--- firebase/index.js
+++ firebase/index.js
@@ -2,7 +2,47 @@ var admin = require("firebase-admin");
 
 var serviceAccount = require('../config/keyFile.json');
 
+const BUCKET = "integrating-with-react.appspot.com";
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  storageBucket: "https://integrating-with-react.firebaseio.com"
+  storageBucket: BUCKET,
 });
+
+const bucket = admin.storage().bucket();
+
+const uploadImage = (req, res, next) => {
+
+  if(!req.file) return next();
+
+  const imagem = req.file;
+
+  const imagemName = Date.now() + "." + imagem.originalname.split('.').pop();
+
+  const file = bucket.file(imagemName);
+
+  const stream = file.createWriteStream({
+    metadata: {
+      contentType: imagem.mimetype,
+    },
+  });
+
+  stream.on("error", (e) => {
+    console.error(e);
+  });
+
+  stream.on("finish", async () => {
+    // Tornar o arquivo publico
+    await file.makePublic();
+
+    //Obter a url publica
+    req.file.firebaseUrl = `https://storage.googleapis.com/${BUCKET}/${imagemName}`;
+
+    next();
+  });
+
+  stream.end(imagem.buffer)
+  
+}
+
+module.exports = uploadImage;
--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,14 @@ const express = require('express');
 const multer = require('multer');
 const routes = express.Router();
 
+const uploadImage = require('../firebase/index');
+
 const Multer = multer({
   storage: multer.memoryStorage(),
   limits: 1024 * 1024,
 });
 
-routes.post('/upload', Multer.single('imagem'), (req, res) =>{
+routes.post('/upload', Multer.single('imagem'), uploadImage, (req, res) =>{
   
   return console.log(req.file);
 
