2ed58bc200c19473a6f435a8b70343bdc89fd7ba
fdAssis <fd07ass@gmail.com>
2020-09-13 15:10:59 -0300

    multer config

--- src/routes.js
+++ src/routes.js
@@ -2,13 +2,17 @@ const express = require('express');
 const multer = require('multer');
 const routes = express.Router();
 
-const multerConfig = require('../config/multer');
-
 const uploadFile = require('../firebase/index');
 
-routes.post('/upload', multer(multerConfig).single('file'), uploadFile, (req, res) =>{
-  
-  return console.log(req.file);
+const Multer = multer({
+  storage: multer.memoryStorage(),
+  limits: 1024 * 1024,
+});
+
+routes.post('/upload', Multer.single('file'), uploadFile, (req, res) =>{
+  const { firebaseUrl} = req.file;
+
+  return res.status(201).json({url : firebaseUrl});
 
 });
 
